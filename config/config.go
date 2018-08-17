1daee8e57ec86ac3758b8a24e16c4a987237f781
Vinicius Luiz <vinicius.luiz@ingresse.com>
2018-08-17 20:18:43 -0300

    commit temporario -> remover depois

--- config/config.go
+++ config/config.go
@@ -2,8 +2,8 @@ package config
 
 const DEV_SERVER_PORT = "5000"
 
-const DB_HOST="fiscaluno_db"
-const DB_NAME="fiscaluno"
-const DB_USER="fiscaluno"
-const DB_PASS="fiscaluno"
+const DB_HOST="ec2-107-21-98-165.compute-1.amazonaws.com"
+const DB_NAME="d8qu9o0hgpq8gt"
+const DB_USER="eoypcdgztrkrwe"
+const DB_PASS="23e466f6aaf8a7bc3bfe055c560e9ed95fd1a0241fddbc36fca90bcba3c0c1c9"
 const DB_PORT="5432"
